199369ce6b53382605ce1c070c3258199b8b30f1
Bruno MC <brunomc@outlook.com>
2021-07-03 15:07:38 -0300

    Update BrunoMC.sol

--- src/contracts/BrunoMC.sol
+++ src/contracts/BrunoMC.sol
@@ -1,22 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts-upgradleable/token/ERC20/ERC20Upgradeable.sol";
-import "@openzeppelin/contracts-upgradleable/proxy/utils/Initializable.sol";
-import "@openzeppelin/contracts-upgradleable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
-contract BrunoMC is ERC20Upgradeable,Initializable, UUPSUpgradeable {
+contract BrunoMC is Initializable, ERC20Upgradeable, UUPSUpgradeable, OwnableUpgradeable {
    function initialize() public initializer  {
          __ERC20_init("BrunoMC", "BMC");
+         __Ownable_init();
         _mint(msg.sender, 1000000 * 10 ** decimals());
     }
-   function _auhtorizeUpgrade(address newLogicContract) internal override onlyOwner {}
+    function _authorizeUpgrade(address) internal override onlyOwner {}
 }
 
 contract BrunoMCV02 is BrunoMC {
-    function version() public view returns (string memory) {
-      return "Version 02";  
+    function getVersion() pure public returns(string memory) {
+        return "V02";
     }
-    
-    //upgradeable logic
 }
